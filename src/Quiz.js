adee1a598b6efee1da9a670df022a1d55b5b71cf
Kelly Murphy <kelly.murphy@bbc.co.uk>
2022-04-08 14:24:30 +0100

    Fix for Creating a Quiz Component

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,13 @@ class Quiz extends Component {
   }
 
   render() {
-    return <div className="QuizQuestion"> {quizData.quiz_questions[0].instruction_text} </div>
+    return (
+      <div>
+        <div className="QuizQuestion">
+          {quizData.quiz_questions[0].instruction_text}
+        </div>
+      </div>
+    );
   }
 }
 
